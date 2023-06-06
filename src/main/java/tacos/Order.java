b1a735af8488d5b0629e25898c654b01d2c860b7
Ferdinand Juraczka <ferdinand.juraczka@contractors.twinformatics.at>
2023-06-06 19:38:00 +0200

    Umstellung auf Spring-Data-JPA und Einbau H2-Datenbank, Anpassung der DB-Felder, Erstellung DB-Sequences

--- src/main/java/tacos/Order.java
+++ src/main/java/tacos/Order.java
@@ -1,29 +1,59 @@
 package tacos;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import org.hibernate.validator.constraints.CreditCardNumber;
 
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 @Data
-public class Order {
+@Entity
+@Table(name="Taco_Order")
+public class Order implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "taco_order_generator")
+    @SequenceGenerator(name = "taco_order_generator", sequenceName = "TACO_ORDER_SEQ", allocationSize = 1)
+    private Long id;
+
+    private Date placedAt;
+
     @NotBlank(message="Name fehlt")
-    private String name;
+    private String deliveryName;
     @NotBlank(message="Straße fehlt")
-    private String street;
+    private String deliveryStreet;
     @NotBlank(message="Ort fehlt")
-    private String city;
+    private String deliveryCity;
     @NotBlank(message="Land fehlt")
-    private String state;
+    private String deliveryState;
     @NotBlank(message="Postleitzahl fehlt")
-    private String zip;
+    private String deliveryZip;
     @CreditCardNumber(message="Kreditkartennummer ist fehlerhaft")
     private String ccNumber;
-    @Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$",
+    @Pattern(regexp="^(0[1-9]|1[0-2])(/)([1-9][0-9])$",
             message="Eingabe nach folgendem Muster: MM/YY")
     private String ccExpiration;
     @Digits(integer=3, fraction=0, message="Falscher CVV-Code")
     private String ccCVV;
+
+    @ManyToMany(targetEntity=Taco.class)
+    private List<Taco> tacos = new ArrayList<>();
+    public void addDesign(Taco design) {
+        this.tacos.add(design);
+    }
+    @PrePersist
+    void placedAt() {
+        this.placedAt = new Date();
+    }
+
 }
--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -1,17 +1,33 @@
-// tag::all[]
-// tag::allButValidation[]
 package tacos;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+
+import java.util.Date;
 import java.util.List;
 
 @Data
+@Entity
 public class Taco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "taco_generator")
+    @SequenceGenerator(name = "taco_generator", sequenceName = "TACO_SEQ", allocationSize = 1)
+    private Long id;
+
     @NotNull
     @Size(min=5, message="Name must be at least 5 characters long")
     private String name;
+
+    private Date createdAt;
+
+    @ManyToMany(targetEntity=Ingredient.class)
     @Size(min=1, message="You must choose at least 1 ingredient")
-    private List<String> ingredients;
+    private List<Ingredient> ingredients;
+
+    @PrePersist
+    void createdAt() {
+        this.createdAt = new Date();
+    }
 }
